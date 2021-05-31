0aed09d071383de58153aa8f252d08963d3fbace
Alexey <alexey.s.matrosov@gmail.com>
2021-05-31 21:29:00 +0300

    add nav icon

--- app/src/main/java/com/smartfoxlabs/fancy/stylish/font/keyboard/ui/components/TabRow.kt
+++ app/src/main/java/com/smartfoxlabs/fancy/stylish/font/keyboard/ui/components/TabRow.kt
@@ -1,13 +1,16 @@
 package com.smartfoxlabs.fancy.stylish.font.keyboard.ui.components
 
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,9 +18,13 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.semantics.Role
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.smartfoxlabs.fancy.stylish.font.keyboard.RallyFontScreen
 
 @Composable
@@ -56,16 +63,28 @@ private fun RallyTab(
 
     Column( verticalArrangement = Arrangement.SpaceEvenly, modifier = Modifier
         .height(TabHeight)
-        .background(Color(0x12))
         .selectable(
             selected = selected,
             onClick = onSelected,
             role = Role.Tab
         )) {
-        Icon(painter = painterResource(id = icon), contentDescription = null, tint = tabTintColor)
-        
-        if (selected) {
+        Box(modifier = Modifier.weight(1f)) {
+            //Icon(painter = painterResource(id = icon), contentDescription = null, tint = tabTintColor)
+            Image(bitmap = ImageBitmap.imageResource(id = icon),
+                contentDescription = null,
+                alignment = Alignment.Center,
+                modifier = Modifier
+                    .size(30.dp, 20.dp))
+            //Icon(bitmap = ImageBitmap.imageResource(id = icon), contentDescription = null, tint = tabTintColor)
+            Text(
+                text = "text",
+                fontSize = 12.sp,
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+            )
+            if (selected) {
 
+            }
         }
     }
 }
